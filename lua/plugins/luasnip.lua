0f8d76b9245e86b7cf200ea8b48958a2faea9a05
FuturePhisicist <1kruglovlev1@gmail.com>
2025-08-05 02:53:26 +0300

    Fixed no snippents

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,8 +1,15 @@
 return {
 	"L3MON4D3/LuaSnip",
+	dependencies = {
+      "rafamadriz/friendly-snippets", -- optional: pre-made snippets
+    },
 	-- follow latest release.
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
-	build = "make install_jsregexp"
+	build = "make install_jsregexp",
+    event = "InsertEnter",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load() -- loads snippets from friendly-snippets or your own paths
+    end,
 }
 
