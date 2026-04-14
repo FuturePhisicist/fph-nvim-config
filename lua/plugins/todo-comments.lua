b6e86920b8dabb004a98ca1a90630cdbbb2785a0
LevKruglov <kruglovlev@star.intra.ispras.ru>
2026-04-14 22:50:50 +0300

    Minor fixes in todo-comments plugin config

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -7,13 +7,14 @@ return {
             TODO = {
                 icon = " ",
                 color = "info",
-                alt = { "MYTODO" },
+                alt = { "MYTODO", "ToDo" },
             },
         },
 		highlight = {
 			before = "", -- "fg" or "bg" or empty
 			keyword = "bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
 			after = "fg", -- "fg" or "bg" or empty
+			comments_only = true,
         },
     },
     config = function(_, opts)
